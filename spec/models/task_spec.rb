5499623f308e643e64062f36e055f361a8b6f48e
nitnek <nitnekb@gmail.com>
2019-05-03 18:52:01 +0200

    test: add request and model tests for task

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -10,9 +10,30 @@ RSpec.describe Task, type: :model do
       expect{ task }.to change{ Task.count }.by 1
     end
 
-    describe "#valid" do
-      it "task" do
-        assert task.valid?
+    describe "#valid?" do
+      let(:task) { build_stubbed(:task) }
+      
+      context "valid" do
+        it "task" do
+          assert task.valid?
+        end
+      end
+      
+      context "invalid" do
+        it "without title" do
+          task.title = nil
+          refute task.valid?
+        end
+
+        it "with title under 3 characters" do
+          task.title = "NO"
+          refute task.valid?
+        end
+
+        it "without state" do
+          task.state = nil
+          refute task.valid?
+        end
       end
     end
   end
